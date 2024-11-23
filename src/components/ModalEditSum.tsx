a21f1d4ae6a05e02622cec803666e9abe59c9ccc
khuzenko <kossguzenko@gmail.com>
2024-11-23 18:37:44 -0500

    added new  budget modal

--- src/components/ModalEditSum.tsx
+++ src/components/ModalEditSum.tsx
@@ -1,39 +1,58 @@
 import {
-  Button,
-  DialogActions,
   DialogContent,
+  DialogFooter,
+  DialogHeader,
   DialogTitle,
-  TextField,
-} from "@mui/material";
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import React from "react";
+import { NumberButtons } from "./NumberButtons";
 
 interface ModalEditSumProps {
   handleClose: () => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 }
 
-export const ModalEditSum = ({ handleClose }: ModalEditSumProps) => {
-  return (
-    <>
-      <DialogTitle>Edit monthly budget</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          required
-          name="month-budget"
-          label="Monthly budget"
-          type="number"
-          fullWidth
-          margin="dense"
-        />
-      </DialogContent>
+export const ModalEditSum = ({ onSubmit }: ModalEditSumProps) => {
+  const refBudget = React.useRef<HTMLInputElement>(null);
 
-      <DialogActions>
-        <Button variant="text" onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button variant="text" type="submit">
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Edit monthly budget</DialogTitle>
+      </DialogHeader>
+
+      <form onSubmit={onSubmit} className="grid gap-4 py-4" id="edit-sum-form">
+        <div className="grid grid-cols-4 items-center gap-2">
+          <Label htmlFor="month-budget" className="text-right">
+            Monthly budget
+          </Label>
+          <Input
+            defaultValue={0}
+            autoFocus
+            required
+            name="month-budget"
+            type="number"
+            className="col-span-3"
+            ref={refBudget}
+          />
+          <div className="col-span-3 col-start-2 flex justify-between">
+            <NumberButtons targetRef={refBudget} />
+          </div>
+        </div>
+      </form>
+
+      <DialogFooter className="grid grid-cols-4 items-center gap-2 mt-2">
+        <Button
+          form="edit-sum-form"
+          type="submit"
+          className="col-span-3 col-start-2"
+        >
           Add
         </Button>
-      </DialogActions>
-    </>
+      </DialogFooter>
+    </DialogContent>
   );
 };
